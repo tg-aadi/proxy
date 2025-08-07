289e0771d98a8c220950e4e1215c889508dc60cf
tg-aadi <rai.09.shivam@gmail.com>
2025-08-07 12:31:47 +0530

    Update proxy.php

--- api/proxy.php
+++ api/proxy.php
@@ -74,7 +74,7 @@ curl_setopt($ch, CURLOPT_URL, $targetUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_NOBODY, false);
+//curl_setopt($ch, CURLOPT_NOBODY, false);
 
 // Forward request method and data
 $method = $_SERVER['REQUEST_METHOD'];
